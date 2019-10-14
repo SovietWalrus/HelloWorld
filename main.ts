bde7a868c2f0b29708c03618f6dc07811073144b
SovietWalrus <20jhudson@slvusd.org>
2019-10-14 11:35:08 -0700

    added a line of text

--- main.ts
+++ main.ts
@@ -3,4 +3,5 @@ brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
     brick.showString("Hello World", 1)
     brick.showString("Everything is Awesome", 2)
     brick.showString("I am You as You are me", 3)
+    brick.showString("Don't you think the joker laughs at you", 4)
 })
