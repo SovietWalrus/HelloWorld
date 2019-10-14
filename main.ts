1aa36f2a75da52672927d68390cc827ad4d87860
SovietWalrus <20jhudson@slvusd.org>
2019-10-14 11:28:11 -0700

    added hello world to hello world

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
+    brick.setStatusLight(StatusLight.GreenFlash)
+    brick.showString("Hello World", 1)
+    brick.showString("Everything is Awesome", 2)
+    brick.showString("I am You as You are me", 3)
+})
